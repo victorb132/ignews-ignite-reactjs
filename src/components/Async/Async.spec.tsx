ee4c60d529e7972282dcc858508cd4e7827909a7
victorb132 <victor96novais@gmail.com>
2021-05-20 13:51:24 -0300

    test coverage

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
+import { Async } from '.'
+
+test('it renders correctly', async () => {
+  render(<Async />)
+
+  expect(screen.getByText('Hello World')).toBeInTheDocument()
+  // expect(await screen.findByText('Button')).toBeInTheDocument()
+
+  // await waitForElementToBeRemoved(
+  //   screen.queryByText('Button')
+  // )
+
+  await waitFor(() => {
+    return expect(screen.getByText('Button')).toBeInTheDocument()
+  })
+
+  // await waitFor(() => {
+  //   return expect(screen.queryByText('Button')).not.toBeInTheDocument()
+  // })
+
+})
